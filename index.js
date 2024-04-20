b05630a018577b763114218e9b47b8cadf728d0f
Your Name <you@example.com>
2024-04-20 09:43:34 +0100

    delete functionality added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 // bJ9oVoxaeqommylM
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://saadealif2010:bJ9oVoxaeqommylM@cluster0.atq5qlj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -42,13 +42,18 @@ async function run() {
             res.send(result);
             console.log(`a new user added . ID : ${result.insertedId}`);
         })
+
+        app.delete('/users/:id' ,async(req,res) =>{
+            const id = req.params.id;
+            console.log("delete button hitting form the client side : ", id);
+            const query = {_id : new ObjectId(id)};
+            const result = await database.deleteOne(query);
+            res.send(result);
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        // Ensures that the client will close when you finish/error
-        // experiment from the mongo community
-
         // await client.close();  ## experiment
     }
 }
